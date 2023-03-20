dff2078eaf3c290401481d54a4c7c960e753087c
PurpleDragon-Tian <tzl0588@163.com>
2023-03-20 15:30:47 +0800

    Initialize nh/kmp_demo

---
+++ Bookshelf/build.gradle.kts
@@ -0,0 +1,25 @@
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+        classpath("com.android.tools.build:gradle:7.1.0-alpha02")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+group = "io.realm.sample.bookshelf"
+version = "0.4.1"
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
